acdd39b9ec4ea62afd44fb9ee48218c0f25796c4
Josh Barrow <jbarrow@decisionpt.com>
2013-04-25 20:41:45 -0500

    Add support for opening .zip and .pkg files from other apps directly on the device

--- app/iEinstein/Classes/AppDelegate.mm
+++ app/iEinstein/Classes/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "iEinsteinViewController.h"
+#import "SSZipArchive.h"
 
 #include "Emulator/JIT/TJITPerformance.h"
 
@@ -10,17 +11,51 @@
 + (void)initialize
 {
     NSDictionary *defaults = @{@"screen_resolution": @0,
-                                @"clear_flash_ram": @NO};
-
+							   @"clear_flash_ram": @NO};
+	
     [[NSUserDefaults standardUserDefaults] registerDefaults:defaults];
     [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    if (url != nil && [url isFileURL]) {
+		NSString *docdir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+		
+		if ([[url pathExtension] isEqualToString:@"zip"] || [[url pathExtension] isEqualToString:@"ZIP"]) {
+			[SSZipArchive unzipFileAtPath:[url absoluteString] toDestination:[docdir stringByAppendingPathComponent:[url lastPathComponent]]];
+		}
+		
+		NSError *error = nil;
+		
+		[[NSFileManager defaultManager] copyItemAtURL:url toURL:[NSURL fileURLWithPath:[docdir stringByAppendingPathComponent:[url lastPathComponent]]] error:&error];
+		
+		if (!error) {
+			UIAlertView *success = [[UIAlertView alloc] initWithTitle:@"Success"
+															  message:[NSString stringWithFormat:@"The file, %@, has been transferred to the package list.", [url lastPathComponent]]
+															 delegate:nil
+													cancelButtonTitle:@"OK"
+													otherButtonTitles:nil, nil];
+			
+			[success show];
+		}
+		else {
+			UIAlertView *success = [[UIAlertView alloc] initWithTitle:@"Error"
+															  message:[NSString stringWithFormat:@"The file, %@, has not been transferred to the package list.", [url lastPathComponent]]
+															 delegate:nil
+													cancelButtonTitle:@"OK"
+													otherButtonTitles:nil, nil];
+			
+			[success show];
+		}
+	}
+    return YES;
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [_window addSubview:[_viewController view]];
     [_window makeKeyAndVisible];
-
+	
 	NSString *docdir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"717006.rom" ofType:nil];
 	NSString *filePath2 = [docdir stringByAppendingPathComponent:@"717006.rom"];
@@ -31,18 +66,18 @@
 	
     NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
     bool clearFlash = [(NSNumber *)[prefs objectForKey:@"clear_flash_ram"] boolValue];
-
+	
     if (clearFlash) {
         [prefs setValue:@NO forKey:@"clear_flash_ram"];
         [prefs synchronize];
         [_viewController stopEmulator];
         [_viewController verifyDeleteFlashRAM:4];
     }
-
+	
 	[SVProgressHUD showWithStatus:@"Loading Emulator..." maskType:SVProgressHUDMaskTypeClear];
-
+	
     [_viewController initEmulator] ? NSLog(@"Succesfully initialized") : NSLog(@"Failure initializing");
-
+	
     return YES;
 }
 
@@ -56,10 +91,10 @@
     if (![_viewController allResourcesFound]) {
         return;
     }
-
+	
     NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
     bool clearFlash = [(NSNumber *)[prefs objectForKey:@"clear_flash_ram"] boolValue];
-
+	
     if (clearFlash) {
         [prefs setValue:@NO forKey:@"clear_flash_ram"];
         [prefs synchronize];
