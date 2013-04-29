f8361f0df45aad0357e56c53d3e5322502aa5cf9
Josh Barrow <jbarrow@decisionpt.com>
2013-04-28 20:14:39 -0500

    Remove uneeded files

--- _Build_/EclipseAndroid/src/org/messagepademu/einstein/constants/OtherConstants.java
+++
@@ -1,7 +0,0 @@
-package org.messagepademu.einstein.constants;
-
-public class OtherConstants {
-	
-	// TODO I'm not sure if this is really a constant FG 2011_11_24?
-	public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
-}
